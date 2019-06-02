a4288b8c81999d766bc1f6117b71bf5f47561f5c
TruthHun <TruthHun@QQ.COM>
2019-06-02 18:49:46 +0800

    分类

--- src/pages/cate/cate.js
+++ src/pages/cate/cate.js
@@ -4,16 +4,21 @@ const app = getApp()
 
 Page({
   data: {
+    categories: {}
   },
   onLoad: function () {
     wx.showLoading({
       title: '玩命加载中...',
     })
+    
+    let that = this;
 
     api.getCategories(function(res){
-      console.log(res);
+      that.setData({
+        categories: res
+      })
     })
-    
+
     wx.hideLoading()
   }
 })
