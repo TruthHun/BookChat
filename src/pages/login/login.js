8799f47c71e109c55ce8770595d7332ecfe9280d
TruthHun <TruthHun@QQ.COM>
2019-06-07 18:23:22 +0800

    用户登录注册页面

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,62 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
+  },
+  wechatLogin: function() {
+    wx.showModal({
+      title: '温馨提示',
+      content: 'BookChat暂未开通微信授权登录，敬请期待',
+    })
+  },
+  toRegister: function() {
+    wx.navigateTo({
+      url: '/pages/reg/reg',
+    })
   }
 })
