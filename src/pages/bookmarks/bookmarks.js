43f98a2ed54e91a3ac4043a44d52668949b8cbd8
TruthHun <TruthHun@QQ.COM>
2019-06-27 22:13:38 +0800

    完成小程序书签管理功能

--- src/pages/bookmarks/bookmarks.js
+++ src/pages/bookmarks/bookmarks.js
@@ -1,66 +1,79 @@
-// pages/bookmarks/bookmarks.js
+const util = require('../../utils/util.js')
+const config = require('../../config.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    bookmarks: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    if (config.debug) console.log("options", options)
+    this.setData({
+      identify: options.identify ? options.identify : ''
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+    this.loadBookmarks()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  loadBookmarks: function() {
+    let that = this
+    if (that.data.identify) {
+      let bookmarks = []
+      util.loading()
+      util.request(config.api.bookmark, {
+        identify: that.data.identify
+      }).then(function(res) {
+        if (config.debug) console.log("config.api.bookmark", res)
+        if (res.data && res.data.bookmarks) {
+          bookmarks = res.data.bookmarks
+        }
+      }).catch(function(e) {
+        console.log(e)
+      }).finally(function() {
+        that.setData({
+          bookmarks: bookmarks,
+          tips: bookmarks.length > 0 ? '' : ' -- 您暂时还有没有书签 -- '
+        })
+        wx.hideLoading()
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  delBookmark: function(e) {
+    if (config.debug) console.log("delBookmark", e)
+    let that = this
+    wx.showModal({
+      title: '温馨提示',
+      content: '您确定要删除该书签吗？',
+      success: function(res) {
+        if (res.confirm) {
+          util.loading("正在删除...")
+          util.request(config.api.bookmark + "?doc_id=" + e.currentTarget.dataset.id, {}, 'DELETE').then(function(res) {
+            if (config.debug) console.log(res)
+          }).catch(function(e) {
+            console.log(e)
+          }).finally(function() {
+            let bookmarks = that.data.bookmarks
+            bookmarks = bookmarks.filter(bookmark => {
+              return bookmark.doc_id != e.currentTarget.dataset.id
+            })
+            that.setData({
+              bookmarks: bookmarks,
+              tips: bookmarks.length > 0 ? '' : ' -- 您暂时还有没有书签 -- ',
+            })
+            wx.hideLoading()
+          })
+        }
+      }
+    })
   }
 })
