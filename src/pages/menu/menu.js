97e59a0b8b9fa0981e0f1ae4f8319cf4e583a8de
TruthHun <TruthHun@QQ.COM>
2019-06-29 14:35:00 +0800

    移除书签

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -8,14 +8,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    book: {},
     menuTree: [],
     token: util.getToken(),
-    book: {},
-    onHide: false,
     identify: '',
-    activeTab: 'menu',
-    bookmark: [],
-    tips: '',
     wd: ''
   },
   /**
@@ -24,72 +20,26 @@ Page({
   onLoad: function(options) {
     let identify = options.id || options.identify || 'dochub';
     if (!identify || identify == undefined) {
-      wx.navigateTo({
+      wx.redirectTo({
         url: '/pages/notfound/notfound',
       })
       return
     }
-    let tab = options.tab || 'menu'
-
     this.setData({
       identify: identify,
-      activeTab: tab
     })
-    if (tab == 'menu') {
-      this.loadData()
-    } else {
-      this.loadBookmark()
-    }
   },
   onShow: function() {
-    if (this.data.onHide) {
-      this.setData({
-        menu: util.menuToTree(util.getStorageMenu()),
-        onHide: false
-      })
-    }
-  },
-  onHide: function() {
-    this.setData({
-      onHide: true
-    })
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+    this.loadData()
   },
   search: function(e) {
     console.log(e)
-  },
-  tabClick: function(e) {
-    this.setData({
-      activeTab: e.detail.tab
-    })
-    let title = ''
-    if (e.detail.tab == 'bookmark') {
-      title = '书签'
-      this.loadBookmark()
-    }else if (e.detail.tab == 'result') {
-      title = '搜索 - ' + this.data.wd
-    } else {
-      title = '目录'
-        this.loadData()
-    }
-    if (title) {
-      wx.setNavigationBarTitle({
-        title: title,
-      })
-    }
-
   },
   loadData: function() {
     let that = this
     let menu = []
     let book = {}
     let identify = that.data.identify
-
     if (that.data.book.book_id > 0) {
       return
     }
@@ -112,48 +62,22 @@ Page({
     }).catch(function(e) {
       console.log(e)
     }).finally(function() {
-      if (menu.length == 0){
+      if (menu.length == 0) {
         wx.redirectTo({
           url: '/pages/notfound/notfound',
         })
         return
       }
       that.setData({
-        menu: util.menuToTree(menu),
+        menuTree: util.menuToTree(menu),
         book: book,
       })
       wx.hideLoading()
-      util.setStorageMenu(menu)
     })
   },
-  loadBookmark: function() {
-    let that = this
-    let bookmark = []
-    wx.setNavigationBarTitle({
-      title: '书签',
-    })
-    if (that.data.bookmark.length == 0) util.loading()
-    util.request(config.api.bookmark, {
-      identify: that.data.identify
-    }).then(function(res) {
-      if (config.debug) console.log("config.api.bookmark", res)
-      if (res.data && res.data.bookmarks) {
-        bookmark = res.data.bookmarks
-      }
-    }).catch(function(e) {
-      console.log(e)
-    }).finally(function() {
-      that.setData({
-        bookmark: bookmark,
-        tips: ' -- 您暂时还有没有书签 -- '
-      })
-      wx.hideLoading()
-    })
-  },
-  itemClick:function(e){
-    console.log(e)
+  itemClick: function(e) {
     wx.navigateTo({
-      url: '/pages/read/read?identify='+e.detail.identify,
+      url: '/pages/read/read?identify=' + e.detail.identify,
     })
   },
   search: function(e) {
@@ -173,16 +97,11 @@ Page({
     }).finally(function() {
       that.setData({
         result: result,
-        activeTab: "result",
         wd: e.detail,
       })
       wx.hideLoading()
       if (result.length == 0) {
         util.toastError("没有搜索到结果")
-      } else {
-        wx.setNavigationBarTitle({
-          title: '搜索 - ' + e.detail,
-        })
       }
     })
   }
