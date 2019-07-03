786f22a71f3043ba3b5ad5b230a181adc7b81e82
TruthHun <TruthHun@QQ.COM>
2019-07-03 21:22:17 +0800

    增加新版本升级提示

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 //app.js
 App({
   onLaunch: function() {
-
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '升级提示',
+              content: '新版本已经为您准备就绪，是否重启升级应用？',
+              success: function (res) {
+                if (res.confirm)  updateManager.applyUpdate()
+              }
+            })
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '温馨提示',
+        content: '您当前微信版本过低，为了您的用户体检，建议您升级到最新版微信'
+      })
+    }
   },
   globalData: {
     userInfo: null
