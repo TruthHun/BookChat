045ef59d20862fd01b4c09fedc2f96845de391e6
TruthHun <TruthHun@QQ.COM>
2019-07-27 11:35:11 +0800

    平板样式兼容

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ let host = 'https://www.bookstack.cn/bookchat'
 
 
 // 是否是调试模式。如果是生产环境，请设置为false
-const debug = true
+const debug = false
 
 // 横幅图片比例，宽高比，小数或者比例(后期从后端接口获取)
 const bannerRatio = 825/315
